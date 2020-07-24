365e64057410b10746d827f1545ff75543cd133d
Harun Shaban <42933471+harunshaban@users.noreply.github.com>
2020-07-24 11:50:49 +0200

    http pack, json manipulation with dummy api

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,30 +7,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-
-    // Simulate network request for a username
-    // Waits until the code below to finish then caryons
-    // If it is not async func it excecutes the code while delay is ongoing
-    String username = await Future.delayed(Duration(seconds: 3), (){
-      return 'yoshi';
-    });
-
-    String bio = await Future.delayed(Duration(seconds: 3), (){
-      return 'vegan, musician & collector';
-    });
-
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    // code caries even if getData is async
-    print('hello world');
-  }
-
   @override
   Widget build(BuildContext context) {
     print('build function ran');
