74e11d8b87f06205a105bbaa96a68e8eefcf7813
Harun Shaban <42933471+harunshaban@users.noreply.github.com>
2020-07-24 22:58:00 +0200

    Custom class geting data successful from api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,28 +8,30 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  String time = 'loading';
 
-  void getData() async {
-
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-
-    // Converts json to map so we can use the fields
-    Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    setupWorldTime();
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
